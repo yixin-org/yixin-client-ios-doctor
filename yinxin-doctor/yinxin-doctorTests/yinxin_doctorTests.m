a1bd3211e1dc44478ac237ea78e3c6aa6b75e145
xuhaohao <xuhaogogo@live.cn>
2014-08-03 20:30:34 -0700

    submit
    user github for mac

---
+++ yinxin-doctor/yinxin-doctorTests/yinxin_doctorTests.m
@@ -0,0 +1,34 @@
+//
+//  yinxin_doctorTests.m
+//  yinxin-doctorTests
+//
+//  Created by dayang on 14-8-3.
+//  Copyright (c) 2014年 goodman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yinxin_doctorTests : XCTestCase
+
+@end
+
+@implementation yinxin_doctorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
