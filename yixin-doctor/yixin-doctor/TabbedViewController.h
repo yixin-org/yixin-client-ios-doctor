5adc9caf92f3fded1f616bcc3b2a58b25640a05f
xuhaohao <xuhaogogo@live.cn>
2014-08-03 20:11:45 -0700

    submit init project
    submit init project

---
+++ yixin-doctor/yixin-doctor/TabbedViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabbedViewController.h
+//  yixin-doctor
+//
+//  Created by dayang on 14-8-3.
+//  Copyright (c) 2014年 goodman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabbedViewController : UIViewController
+
+@end
---
+++ yixin-doctor/yixin-doctor/TabbedViewController.m
@@ -0,0 +1,29 @@
+//
+//  TabbedViewController.m
+//  yixin-doctor
+//
+//  Created by dayang on 14-8-3.
+//  Copyright (c) 2014年 goodman. All rights reserved.
+//
+
+#import "TabbedViewController.h"
+
+@interface TabbedViewController ()
+
+@end
+
+@implementation TabbedViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
