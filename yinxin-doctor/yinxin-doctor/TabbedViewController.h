5c0dc6d036c8e798bfdf6c4d086c501f1869fd97
xuhaohao <xuhaogogo@live.cn>
2014-08-03 20:30:01 -0700

    submit

---
+++ yinxin-doctor/yinxin-doctor/TabbedViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabbedViewController.h
+//  yinxin-doctor
+//
+//  Created by dayang on 14-8-3.
+//  Copyright (c) 2014年 goodman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabbedViewController : UIViewController
+
+@end
